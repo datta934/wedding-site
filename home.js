ca6017db9a96a4155ac976a468f1a5f9b75642bb
mauaa <mauaa@koder.com>
2020-05-03 21:27:06 +0530

    Added source

---
+++ home.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('.parallax').parallax();
+
+    var countDownDate = new Date("Jan 20, 2020 8:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            $("#save-the-date, #hide-text").addClass("hide");
+            document.getElementById("wedding-date").innerHTML = "Can't keep calm! It's our wedding today!";
+        }
+    });
+});
